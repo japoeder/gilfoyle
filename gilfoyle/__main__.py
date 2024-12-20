0e583ffb49a3b64efabdae826c5d020863595ad9
japoeder <japoeder@gmail.com>
2024-12-20 00:03:10 -0600

    adding utilities, update linter, update pinned python version.

--- gilfoyle/__main__.py
+++ gilfoyle/__main__.py
@@ -1,33 +1,38 @@
-from flask import Flask, request, jsonify
-from dotenv import load_dotenv
-load_dotenv()
+"""
+Main entry point for Gilfoyle.
+"""
+
 import sys
 import os
+import signal
+from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Add the parent directory to sys.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from gilfoyle.ticker_expansion import ticker_expansion
-import logging
-import pandas as pd
-import argparse
-import asyncio
-
+from gilfoyle._utils.logging_config import setup_logging
 
 app = Flask(__name__)
-# Configure logging
-# logging.basicConfig(filename=f'app.log', level=logging.DEBUG,
-#                     format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
-# # Add console handler for logging
-# console_handler = logging.StreamHandler()
-# console_handler.setLevel(logging.DEBUG)
-# formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
-# console_handler.setFormatter(formatter)
-# logging.getLogger().addHandler(console_handler)
 
-# logging.debug("This is a test log message.")
+def handle_sigterm(*args):
+    """Handle SIGTERM signal."""
+    print("Received SIGTERM, shutting down gracefully...")
+    # Perform any cleanup here
+    sys.exit(0)
 
-#TODO: update to return meta data in response (can prob. use timestame to control)
+
+signal.signal(signal.SIGTERM, handle_sigterm)
+
+# Set up logging configuration
+setup_logging()
+
+
+# TODO: update to return meta data in response (can prob. use timestame to control)
 @app.route("/method_1", methods=["POST"])
 def run_expansion():
     """Endpoint to load a new stock ticker into the database."""
@@ -35,12 +40,18 @@ def run_expansion():
     ticker_symbol = data.get("ticker_symbol")
     collection_name = data.get("collection_name", "rawPriceColl")
     ticker_expansion()
-    return jsonify({"status": f"{ticker_symbol} dataframe loaded into {collection_name} collection."}), 202
-
+    return (
+        jsonify(
+            {
+                "status": f"{ticker_symbol} dataframe loaded into {collection_name} collection."
+            }
+        ),
+        202,
+    )
 
 
 if __name__ == "__main__":
-    #app.run(debug=True, host="0.0.0.0", port=8001)
+    # app.run(debug=True, host="0.0.0.0", port=8001)
     ticker_expansion()
     # load_historical_quote_alpacaAPI(
     #     ticker_symbol='AAPL',
